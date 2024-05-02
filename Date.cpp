0232ebf912fda9559ce140c9fe7b5022add63b59
killrusxlf <excgerty@gmail.com>
2024-05-02 04:21:34 +0200

    beta

---
+++ Date.cpp
@@ -0,0 +1,16 @@
+#include "Date.h"
+
+int& Date::operator[](int index)
+{
+    if (index < 0 || index > 2)
+    {
+        throw out_of_range("Index is out of range.");
+    }
+
+    switch (index) {
+    case 0: return day;
+    case 1: return month;
+    case 2: return year;
+    default: return day;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <algorithm>
-#include <string>
-
+#include "Date.cpp"
 
 using namespace std;
 
+istream& operator>>(istream& in, Date& date) 
+{
+    in >> date.day >> date.month >> date.year;
+    return in;
+}
+
+ostream& operator<<(ostream& out, const Date& date) 
+{
+    out << date.day << "." << date.month << "." << date.year;
+    return out;
+}
 
 
 int main() {
-    
-           
+    Date d1; 
+    Date d2(02, 05, 2024);
+    Date d3(d2); 
+
+    cout << d1[0] << "." << d1[1] << "." << d1[2] << endl; 
+
+    int day = d3();
+    cout << "The day from d3 is " << day << endl;
+    Date d4;
+    cin >> d4;
+    cout << d4 << endl;
+
     return 0;
 }
-
